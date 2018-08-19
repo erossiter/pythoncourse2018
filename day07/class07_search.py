88fc23fcc5b8fbd8bde18f421310e92cc1ccc5f0
erossiter <erinrossiter@psci017als17.wustl.edu>
2018-08-19 15:30:10 -0500

    Day7 ready

---
+++ day07/class07_search.py
@@ -0,0 +1,40 @@
+## returns element if it is in the list
+def linear_search(mylist, element):
+  steps = 0
+  for item in mylist:
+    steps += 1
+    if item == element:
+      print steps
+      return item
+  print steps
+  return None
+
+
+mylist = range(25)
+
+linear_search(mylist, 1)
+linear_search(mylist, 5)
+linear_search(mylist, 10)
+
+
+
+## returns element if it is in sorted list
+def binary_search(sorted_list, element):
+  print "Input list is {0}".format(sorted_list)
+  print "Input size is {0}".format(len(sorted_list))
+  middle = len(sorted_list)/2
+  median = sorted_list[middle]
+  if len(sorted_list)<=1:
+    if element == median:
+      return median
+    else:
+      return None
+  if element < median:
+    left = sorted_list[0:middle]
+    return binary_search(left, element)
+  else: 
+    right = sorted_list[middle:]
+    return binary_search(right, element)
+
+mylist = range(25)
+binary_search(mylist, 10)
---
+++ day07/lab07.py
@@ -0,0 +1,12 @@
+## Exercise 1
+## Write a function using recursion to calculate the greatest common divisor of two numbers
+
+## Helpful link:
+## https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+def gcd(x, y):
+
+
+## Exercise 2
+## Write a function using recursion that returns prime numbers less than 121
+def find_primes(me = 121, primes = []):
+ 
